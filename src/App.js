8116c75be8b4e3be0f65147999eba323649718f7
Adela <adelangr822@gmail.com>
2022-08-30 17:49:38 +0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./components/Login";
-import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
